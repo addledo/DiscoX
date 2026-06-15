d124a0c2f28da6defbbbbc6cc7ccb0f8a28417c4
addledo <jontyfpine@gmail.com>
2026-06-15 20:35:49 +0100

    No format tests

--- C++/format.sh
+++ C++/format.sh
@@ -10,6 +10,6 @@ fi
 while IFS= read -r -d '' file; do
     echo "Formatting $file"
     clang-format -i "$file"
-done < <(find . -name '.pio' -prune -o \( -name "*.cpp" -o -name "*.h" \) -print0)
+done < <(find . -name '.pio' -prune -o -name 'test' -prune -o \( -name "*.cpp" -o -name "*.h" \) -print0)
 
 echo "Done"
